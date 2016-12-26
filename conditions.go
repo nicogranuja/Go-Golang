b8a5ea40739b944e7c6171d53a975811f5a881e2
Nicolas Alvarez Mondragon <nicogranuja@hotmail.com>
2016-12-26 11:21:09 -0600

    conditions and rules for them

---
+++ conditions.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := 10
+	y := 5
+
+	//needs parenthesis and brackets on same line
+	//parenthesis not necessary 
+	if (x > y){
+		fmt.Println("x is greater than y")
+	}else if x < y{
+		fmt.Println("y is greater than x")
+	}else{
+			
+		}
+	
+}
