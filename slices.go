4fda67e876783af6d754b8c60da5f01e3eeb6828
Nicolas Alvarez Mondragon <nicogranuja@hotmail.com>
2016-12-26 20:01:21 -0600

    added slices

--- slices.go
+++ slices.go
@@ -5,5 +5,19 @@ import(
 )
 
 func main() {
-	
+	//creating slice leaving brackets empty
+	//slice created with null value "nil"
+	matrix := [] int{1,2,3,4}
+
+	fmt.Println(matrix)
+
+	//creating array
+	arr := [3] int {1,2,3}
+
+	//converting to slice
+	//slicing the array from index
+	//prints [1 2] from "0 to 1" index
+	slice := arr[:2]
+
+	fmt.Println(slice)
 }
