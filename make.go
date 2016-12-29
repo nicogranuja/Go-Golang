d550c01ff6e3f349f2120a42ee415381df9dd23b
Nicolas Alvarez Mondragon <nicogranuja@hotmail.com>
2016-12-28 22:16:54 -0600

    added make for slices

---
+++ make.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	//creating a slice with the make method
+	//make (type, length, capacity)
+	slice := make([]int, 3, 5)
+
+	//cap function returns the capacity
+	fmt.Println(cap(slice))
+
+	slice = append(slice, 2)
+	
+	//capacity is 5
+	fmt.Println(cap(slice))
+	//length is 4
+	fmt.Println(len(slice))
+
+	fmt.Println(slice)
+
+}
